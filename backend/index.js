79d241608c4bc639d92454d8d1e4bf599279234d
unknown <ayushiranjana@gmail.com>
2022-01-20 15:20:20 +0530

    created login/register api endpoints

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,33 @@
-const express = require('express');
+// File : index.js
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const user = require("./routes/user"); //new addition
+const InitiateMongoServer = require("./config/db");
+
+// Initiate Mongo Server
+InitiateMongoServer();
+
 const app = express();
 
-require('dotenv').config();
+// PORT
+const PORT = process.env.PORT || 4000;
 
-// database connection
+// Middleware
+app.use(bodyParser.json());
 
-require('./config/database');
-
-app.get('/', (req,res)=>{
-    res.send("API Dashboard");
-})
+app.get("/", (req, res) => {
+  res.json({ message: "API Working" });
+});
 
 
-app.listen(process.env.PORT, ()=>{
-    console.log("Server listening on port " + process.env.PORT);
-})
+/**
+ * Router Middleware
+ * Router - /user/*
+ * Method - *
+ */
+app.use("/user", user);
+
+app.listen(PORT, (req, res) => {
+  console.log(`Server Started at PORT ${PORT}`);
+});
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+          <nav className="navbar navbar-light bg-light">
+    <div className="container">
+    <a className="navbar-brand" href="/">Cuvette</a>
+    <div className="d-flex">
+     
+      <button className="btn btn-primary" type="submit">Login/Signup</button>
+    </div>
+  </div>
+</nav>
+
+        </div>
+    )
+}
+
+export default Navbar
